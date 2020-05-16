fc0c195274e8c617015555c90037bf73736a60bd
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-05-15 22:26:55 -0400

    adding useform and uselocalstorage

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,37 @@
-// write your custom hook here to control your checkout form
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useForm = (initialState, key, submitLogic, notStickyArray) => {
+  // const [username, setUsername] = useState("");
+  // const [email, setEmail] = useState("");
+  const [values, setValues] = useLocalStorage(
+    initialState,
+    key,
+    notStickyArray
+  );
+
+  const handleChanges = e => {
+    // setUsername(e.target.value);
+    // access the field name with e.target.name
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  // const handleEmailChanges = e => {
+  //   setEmail(e.target.value);
+  // };
+
+  const clearForm = () => {
+    setValues(initialState);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    submitLogic();
+    clearForm();
+    // send to api server to update
+  };
+
+  return [values, handleChanges, clearForm, handleSubmit];
+};
