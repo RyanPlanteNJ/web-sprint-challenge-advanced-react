496832266196a0039b969a3498bb62561a4e8bc0
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-05-16 00:01:36 -0400

    MVP met

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,37 +1,32 @@
 import { useLocalStorage } from "./useLocalStorage";
+import React, { useState } from "react";
 
-export const useForm = (initialState, key, submitLogic, notStickyArray) => {
+export const useForm = (initialState, key) => {
   // const [username, setUsername] = useState("");
   // const [email, setEmail] = useState("");
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [values, setValues] = useLocalStorage(
     initialState,
-    key,
-    notStickyArray
+    key
   );
 
   const handleChanges = e => {
-    // setUsername(e.target.value);
-    // access the field name with e.target.name
     setValues({
       ...values,
       [e.target.name]: e.target.value
     });
   };
 
-  // const handleEmailChanges = e => {
-  //   setEmail(e.target.value);
-  // };
 
   const clearForm = () => {
     setValues(initialState);
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    submitLogic();
+    setShowSuccessMessage(true);
     clearForm();
-    // send to api server to update
-  };
+   };
 
   return [values, handleChanges, clearForm, handleSubmit];
 };
